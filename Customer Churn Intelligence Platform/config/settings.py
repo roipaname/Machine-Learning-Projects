2929bf82edc50440dcab653901f7e1c0f9ffc26e
roipaname <roipaname@gmail.com>
2026-01-21 22:35:33 +0200

    More changes

--- Customer Churn Intelligence Platform/config/settings.py
+++ Customer Churn Intelligence Platform/config/settings.py
@@ -2,3 +2,46 @@ import os
 import sys
 from pathlib import Path
 from dotenv import load_dotenv
+from loguru import logger
+from typing import Dict,List
+# Load environment variables from .env file
+load_dotenv()
+# ============================================================================
+# PROJECT PATHS
+# ============================================================================
+
+# Project root directory
+BASE_DIR=Path(__file__).resolve().parent.parent
+DATA_DIR=BASE_DIR / 'data'
+MODELS_DIR=BASE_DIR / 'models'
+LOGS_DIR=BASE_DIR  /' logs'
+CONFIG_DIR=BASE_DIR / 'config'
+DATA_RAW_DIR=DATA_DIR /'raw'
+DATA_PROCESSED_DIR=DATA_DIR /'processed'
+DATA_FEATURE_DIR=DATA_DIR /'feature store'
+KAGGLE_TRAIN_DATASET=DATA_DIR / 'raw/training.csv'
+KAGGLE_TEST_DATASET=DATA_DIR / 'raw/test.csv'
+
+
+for directory in [DATA_DIR, MODELS_DIR, LOGS_DIR,DATA_RAW_DIR,DATA_PROCESSED_DIR,DATA_FEATURE_DIR]:
+    directory.mkdir(parents=True, exist_ok=True)
+
+# ============================================================================
+# DATABASE SET UP
+# ============================================================================
+
+
+DB_NAME=os.getenv("customerchurn")
+
+if not DB_NAME:
+    logger.error("DB_NAME not set")
+    raise
+DB_USER=os.getenv("DB_USER")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
+DB_HOST=os.getenv("DB_HOST","localhost")
+DB_PORT=os.getenv("DB_PORT","5432")
+
+DB_POOL_SIZE = int(os.getenv('DB_POOL_SIZE', '10'))
+DB_MAX_OVERFLOW = int(os.getenv('DB_MAX_OVERFLOW', '20'))
+DB_POOL_TIMEOUT = int(os.getenv('DB_POOL_TIMEOUT', '30'))
+DB_ECHO = os.getenv('DB_ECHO', 'False').lower() == 'true'
