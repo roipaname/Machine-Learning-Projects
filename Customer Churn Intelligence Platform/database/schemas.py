bffc797e756ea137e896e480d8b021673a0d4a5d
roipaname <roipaname@gmail.com>
2026-01-22 21:33:36 +0200

    almost done setting up

--- Customer Churn Intelligence Platform/database/schemas.py
+++ Customer Churn Intelligence Platform/database/schemas.py
@@ -16,6 +16,33 @@ class AccountTier(enum.Enum):
     silver="silver"
     gold="gold"
 
+class AcquisitionChannel(enum.Enum):
+    ads="ads"
+    referral="referral"
+    sales="sales"
+    organic="organic"
+
+class CustomerSegment(enum.Enum):
+    SMB="SMB"
+    Midmarket="Midmarket"
+    Enterprise="Enterprise"
+
+class SubPlanName(enum.Enum):
+    basic="basic"
+    pro="pro"
+    enterprise="enterprise"
+class  SubscriptionStatus(enum.Enum):
+    active="active"
+    cancelled="cancelled"
+    paused="paused"
+
+class EventType(enum.Enum):
+    login="login"
+    apicall="apicall"
+    upload="upload"
+    export="export"
+
+
 
 Base=declarative_base()
 
@@ -42,3 +69,50 @@ class Customers(Base):
     __tablename__="customers"
     customer_id=Column(UUID(as_uuid=True),primary_key=True,unique=True,default=uuid.uuid4)
     account_id=Column(UUID(as_uuid=True),ForeignKey("accounts.account_id",ondelete="CASCADE"))
+    first_name = Column(Text, nullable=True)
+    last_name = Column(Text, nullable=True)
+    email = Column(Text, nullable=False, unique=True)
+    country = Column(Text, nullable=True)
+
+    signup_date = Column(DateTime, nullable=False, default=datetime.utcnow)
+    acquisition_channel = Column(Enum(
+        AcquisitionChannel,"acquisition_channel_enum"
+    ), nullable=True)
+    customer_segment = Column(Enum(
+        CustomerSegment,"customer_segmnet_enum"
+    ), nullable=True)
+    __table_args__=(
+        Index("idx_customer_acquisition_channel","acquisition_channel"),
+        Index("idx_customer_customer_segment","customer_segment"),
+        Index("idx_customer_signup_date","signup_date")
+    )
+
+
+class Subscriptions(Base):
+    __tablename__="subscriptions"
+    subscription_id=Column(UUID(as_uuid=True),primary_key=True,unique=True,default=uuid.uuid4)
+    account_id=Column(UUID(as_uuid=True),ForeignKey("accounts.account_id",ondelete="CASCADE"))
+    plan_name=Column(Enum(SubPlanName,"subplan_name_enum"),nullable=False,default="basic")
+    monthlyfee=Column(Float,nullable=False)
+    start_date=Column(DateTime, nullable=False, default=datetime.utcnow)
+    end_date=Column(DateTime, nullable=False)
+    status=Column(Enum(SubscriptionStatus,"sub_satus_enum"),nullable=False,default="active")
+    __table_args__=(
+        Index("idx_subscription_plan_name","plan_name"),
+        Index("idx_subscription_status","status")
+    )
+
+
+class UsageEvents(Base):
+    __tablename__="usage_events"
+    event_id=Column(UUID(as_uuid=True),primary_key=True,unique=True,default=uuid.uuid4)
+    customer_id=Column(UUID(as_uuid=True),ForeignKey("customers.customer_id",ondelete="CASCADE"))
+    event_type=Column(Enum(
+        EventType,"event_type_enum"
+    ),nullable=False, default="apicall")
+    device_type=Column(Text,nullable=True, default="phone")
+    __table_args__=(
+        Index("idx_usage_events_event_type","event_type"),
+        Index("idx_usage_events_device_type","device_type")
+    )
+
