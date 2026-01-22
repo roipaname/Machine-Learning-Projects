66d4081daa91c1ae519f703cad4326beff9b8fed
roipaname <roipaname@gmail.com>
2026-01-22 15:47:51 +0200

    Doing Imports

--- Customer Churn Intelligence Platform/database/schemas.py
+++ Customer Churn Intelligence Platform/database/schemas.py
@@ -1,1 +1,10 @@
-from sqlalchemy 
+from sqlalchemy import Column,Text,Integer,Float,DateTime,Boolean,Index,UniqueConstraint
+from sqlachemy.ext.declarative import declarative_base
+from slqalchemy.dialects.postgresql import JSON,UUID
+
+from datetime import datetime
+from  loguru import logger
+import uuid
+
+
+base=declarative_base()
