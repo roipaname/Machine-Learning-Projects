154d1a12381c24d6ff3972e6a9799a5f9173584f
roipaname <roipaname@gmail.com>
2026-01-25 20:59:13 +0200

    inserting accounts

--- Customer Churn Intelligence Platform/database/insert_data.py
+++ Customer Churn Intelligence Platform/database/insert_data.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from config.settings import DATA_PROCESSED_DIR
 from loguru import logger
+from database.operations import insert_new_account
 
-accounts_path=DATA_PROCESSED_DIR / 'accounts.csv'
+DATA_PROCESSED_CSV=DATA_PROCESSED_DIR /'csv'
+accounts_path=DATA_PROCESSED_CSV/ 'accounts.csv'
 
 def read_and_store_accounts(acc_path:Path):
     if not acc_path or not acc_path.exists():
@@ -14,6 +16,11 @@ def read_and_store_accounts(acc_path:Path):
 
     df=pd.read_csv(acc_path)
     print(df.info())
+    logger.info(f"inserting {len(df)} accouns into database")
+
+    for idx,row in df.iterrows():
+        insert_new_account(row.to_dict())
+    logger.success("Done inserting Accounts")
 
 
 read_and_store_accounts(accounts_path)
