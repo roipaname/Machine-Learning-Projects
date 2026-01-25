9bf2ab812fb6e83c45bae25757c055e141c3b9c5
roipaname <roipaname@gmail.com>
2026-01-25 21:03:52 +0200

    inserting customer

--- Customer Churn Intelligence Platform/database/insert_data.py
+++ Customer Churn Intelligence Platform/database/insert_data.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from config.settings import DATA_PROCESSED_DIR
 from loguru import logger
-from database.operations import insert_new_account
+from database.operations import insert_new_account,insert_customer
 
 DATA_PROCESSED_CSV=DATA_PROCESSED_DIR /'csv'
 accounts_path=DATA_PROCESSED_CSV/ 'accounts.csv'
+customer_path=DATA_PROCESSED_CSV/ 'customers.csv'
 
 def read_and_store_accounts(acc_path:Path):
     if not acc_path or not acc_path.exists():
@@ -19,8 +20,28 @@ def read_and_store_accounts(acc_path:Path):
     logger.info(f"inserting {len(df)} accouns into database")
 
     for idx,row in df.iterrows():
-        insert_new_account(row.to_dict())
+        try:
+            insert_new_account(row.to_dict())
+        except Exception as e:
+            logger.error(f"Failed to insert account {row['account_id']}")
     logger.success("Done inserting Accounts")
 
+def read_and_store_cusomers(customer_path:Path):
+    if not customer_path or not customer_path.exists():
+        logger.error("Path not passed or found")
+        raise
 
-read_and_store_accounts(accounts_path)
+    df=pd.read_csv(customer_path)
+    print(df.info())
+    logger.info(f"inserting {len(df)} customers into database")
+
+    for idx,row in df.iterrows():
+        try:
+            insert_customer(row.to_dict())
+        except Exception as e:
+            logger.error(f"Failed to insert customer {row['customer_id']}")
+    logger.success("Done inserting Accounts")
+
+#read_and_store_accounts(accounts_path)
+
+read_and_store_cusomers(customer_path)
