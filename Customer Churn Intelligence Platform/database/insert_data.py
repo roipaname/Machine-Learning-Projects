b047624d9bcf5ffdfcd6493a69ed1e52022d9fe0
roipaname <roipaname@gmail.com>
2026-01-25 21:19:28 +0200

    inserting subs

--- Customer Churn Intelligence Platform/database/insert_data.py
+++ Customer Churn Intelligence Platform/database/insert_data.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from config.settings import DATA_PROCESSED_DIR
 from loguru import logger
-from database.operations import insert_new_account,insert_customer
+from database.operations import insert_new_account,insert_customer,create_subscription,create_support_ticket
 
 DATA_PROCESSED_CSV=DATA_PROCESSED_DIR /'csv'
 accounts_path=DATA_PROCESSED_CSV/ 'accounts.csv'
 customer_path=DATA_PROCESSED_CSV/ 'customers.csv'
+subscription_path=DATA_PROCESSED_CSV / 'subscriptions.csv'
 
 def read_and_store_accounts(acc_path:Path):
     if not acc_path or not acc_path.exists():
@@ -42,6 +43,26 @@ def read_and_store_cusomers(customer_path:Path):
             logger.error(f"Failed to insert customer {row['customer_id']}")
     logger.success("Done inserting Accounts")
 
+def read_and_store_subs(subpath:Path):
+    if not subpath or not subpath.exists():
+        logger.error("Path not passed or found")
+        raise
+    df= pd.read_csv(subpath)
+    print(df.info())
+    logger.info(f"inserting {len(df)} subs into database")
+
+    for idx ,row in df.iterrows():
+        try:
+            create_subscription(row.to_dict())
+        except Exception as e:
+            logger.error(f"failed to insert sub id {row['subscription_id']}")
+            raise
+        logger.success("Done inserting Subscriptions")
+    
+
+
 #read_and_store_accounts(accounts_path)
 
-read_and_store_cusomers(customer_path)
+#read_and_store_cusomers(customer_path)
+
+read_and_store_subs(subscription_path)
