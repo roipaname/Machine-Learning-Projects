43168d222765fc81fa4a837b935bbd4923f01303
roipaname <roipaname@gmail.com>
2026-02-08 16:55:32 +0200

    function to train model

--- Customer Churn Intelligence Platform/scripts/train_data.py
+++ Customer Churn Intelligence Platform/scripts/train_data.py
@@ -1,4 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.models.classifier import ChurnPredictor
+from config.settings import DATA_PROCESSED_DIR
 
+data_parquet_path=DATA_PROCESSED_DIR /'training_features.parquet'
+
+def train_model(data_path):
+    #load dataset
+    df=pd.read_parquet(data_path)
